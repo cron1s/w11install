7ac4089009a4c5f2fa6669026934e991f32f15ae
cron1s <78734417+cron1s@users.noreply.github.com>
2022-09-21 23:24:09 +0200

    Delete 01_set-policy.cmd

--- 01_set-policy.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList 'Set-ExecutionPolicy Bypass -Force' -Verb RunAs}"
-Sleep 4
-shutdown -r -t 0
