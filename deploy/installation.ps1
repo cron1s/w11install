0c3655517fe13309da3d9b384487f0a020ca1b20
cron1s <78734417+cron1s@users.noreply.github.com>
2022-09-25 22:19:17 +0200

    Removed 1-3 Steps
    Moved 1-3 Steps into installation "Step 1" for better QoS.
    No bugchecking done...this build could experience some problems. Doing this on another patch

--- deploy/installation.ps1
+++ deploy/installation.ps1
@@ -40,56 +40,43 @@ function Show-Menu {
     Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem
     Write-Host ............................................................
     Write-Host "================ $Title ================"
-    Write-Host "1: Press '1' to Add Hostname"
-    Write-Host "2: Press '2' to Join Domain"
-    Write-Host "3: Press '3' to Join Workgroup"
-    Write-Host "4: Press '4' for Step 1"
-    Write-Host "5: Press '5' for Step 2"
-    Write-Host "6: Press '6' for Step 3"
+    Write-Host "1: Press '1' to Step 1"
+    Write-Host "2: Press '2' to Step 2"
+    Write-Host "3: Press '3' to Step 3"
+    #Write-Host "4: Press '4' for Step 1"
+    #Write-Host "5: Press '5' for Step 2"
+    #Write-Host "6: Press '6' for Step 3"
     Write-Host "Q: Press 'Q' to quit."
     Write-Host
 }
 
-Function Add_Hostname {
-    $NewComputerName = Read-Host [Enter new hostname]
-    Rename-Computer -NewName $NewComputerName
-    Write-Host "Script-INFO: Restarting..." -ForegroundColor white -BackgroundColor green
-    Restart-Computer -Force -Wait 5
-}
-Function Join_Domain {
-    Write-Host "Script-INFO: Join domain" -ForegroundColor white -BackgroundColor green
-    $NewDomain = Read-Host [Enter new domain]
-    Add-Computer -DomainName $NewDomain
-    Write-Host "Script-INFO: Restarting..." -ForegroundColor white -BackgroundColor green
-    Restart-Computer -Force -Wait 5
-}
-Function Join_Workgroup {
-    Write-Host "Script-INFO: Joining Workgroup" -ForegroundColor white -BackgroundColor green
-    $NewDomain = "workgroup"
-    Add-Computer -DomainName $NewDomain
-    Write-Host "Script-INFO: Restarting..." -ForegroundColor white -BackgroundColor green
-    Restart-Computer -Force -Wait 5
-}
 Function First_Step {
-    
-    Write-Host "For this Step you need following:"
-    Write-Host "- You've joined the domain (Currently set: $Domainname"
-    Write-Host "- You've set the Computername (Currently set: $Computername)"
-    Write-Host "- The local Administrator-Password"
-    Write-Host ...............................................................................
-    $confirmation = Read-Host "If you did all these steps above, you can answer 'y' for yes:"
-    if ($confirmation -ne 'y') {
-        break
-    }
 
+    Write-Host "Before the script can start, it needs some informations:"
+    Write-Host ...............................................................................
+    Write-Host "Domainname (Currently set: $Domainname"
+    $NewDomainName = Read-Host "Enter the new Domainname:"
+    Write-Host ...............................................................................
+    Write-Host "Computername (Currently set: $Computername)"
+    $NewComputerName = Read-Host "Enter the new Computername:"
+    Write-Host ...............................................................................
+    Write-Host "The local Administrator-Password"
+    $Admin_Pass_PA = Read-Host -AsSecureString "Computername - $NewComputerName `nEnter Password for Administrator"
+    Write-Host ...............................................................................
+    Clear-Host
+    Write-Host ...........................................................................................................
+    Write-Host "The Script will begin the Setup now."
+    Write-Host ...........................................................................................................
+    Start-Sleep 3
+    
 
 
     if (Test-Path -Path $TempDIR){
-        Write-Host "Script-INFO: C:\Temp exists" -ForegroundColor Green
+        Write-Host "C:\Temp exists" -ForegroundColor Green
     }
     else {
         New-Item $TempDIR -ItemType Directory
-        Write-Host "Script-INFO: C:\Temp Directory created" -ForegroundColor Green
+        Write-Host "C:\Temp Directory created" -ForegroundColor Green
     }
     if (Test-Path -Path $TempDIRFA){
     }
@@ -109,28 +96,28 @@ Function First_Step {
     
     #Start.bin Configuration on Local Logged-On User and Default User
     try {
-        Write-Host "Script-Info: Getting Modification File for the Start Layout" -ForegroundColor white -BackgroundColor Green
+        Write-Host "Getting Modification File for the Start Layout" -ForegroundColor white -BackgroundColor Green
         $client = new-object System.Net.Webclient
         $client.DownloadFile("$Start_bin_Source", "$TempDIRFADATA\Pack.zip")
         Expand-Archive -Path "$TempDIRFADATA\pack.zip" -DestinationPath $TempDIRFADATA -Force
         
         Start-Sleep 10
-        Write-Host "Script-Info: Modification File Download success" -ForegroundColor white -BackgroundColor Green
-        Write-Host "Script-Info: Beginning to applying start.bin" -ForegroundColor white -BackgroundColor Green
+        Write-Host "Modification File Download success" -ForegroundColor white -BackgroundColor Green
+        Write-Host "Beginning to applying start.bin" -ForegroundColor white -BackgroundColor Green
         $Current_User_ModificationFile = "C:\Users\$Current_User_Name"
         try {
-            Write-Host "Script-Info: Beginning to applying start.bin to DefaultUser" -ForegroundColor Green
+            Write-Host "Beginning to applying start.bin to DefaultUser" -ForegroundColor Green
             robocopy "$Start_bin_Dir" "$Default_User_ModificationFile\$Appdata_Start_bin_Dir" /MIR /LOG:$TempDIRFADATA\log_default.txt
         }
         catch {
-            Write-Host "Script-Info: Error applying start_bin to DefaultUser" -ForegroundColor white -BackgroundColor Red
+            Write-Host "Error applying start_bin to DefaultUser" -ForegroundColor white -BackgroundColor Red
         }
         try {
-            Write-Host "Script-Info: Beginning to applying start.bin to CurrentUser" -ForegroundColor Green
+            Write-Host "Beginning to applying start.bin to CurrentUser" -ForegroundColor Green
             robocopy "$Start_bin_Dir" "$Current_User_ModificationFile\$Appdata_Start_bin_Dir" /MIR /LOG:$TempDIRFADATA\log_current.txt
         }
         catch {
-            Write-Host "Script-Info: Error applying start_bin to CurrentUser" -ForegroundColor white -BackgroundColor Red
+            Write-Host "Error applying start_bin to CurrentUser" -ForegroundColor white -BackgroundColor Red
         }
         try {
             if (Test-Path -Path "$User_Dir\Administrator"){
@@ -138,54 +125,69 @@ Function First_Step {
             else {
                 New-Item $Admin_Dir -ItemType Directory 
             }
-            Write-Host "Script-Info: Beginning to applying start.bin to AdministratorUser" -ForegroundColor Green
+            Write-Host "Beginning to applying start.bin to AdministratorUser" -ForegroundColor Green
             robocopy "$Admin_User_ModificationFile" "$User_Dir" /MIR /LOG:$TempDIRFADATA\log_admin.txt
         }
         catch {
-            Write-Host "Script-Info: Error applying start_bin to AdministratorUser" -ForegroundColor white -BackgroundColor Red
+            Write-Host "Error applying start_bin to AdministratorUser" -ForegroundColor white -BackgroundColor Red
         }
         
 
     }
     catch {
-        Write-Host "Script-INFO: Modification File is not available. Exiting Modification" -ForegroundColor white -BackgroundColor red
+        Write-Host "Modification File is not available. Exiting Modification" -ForegroundColor white -BackgroundColor red
     }
 
     #Activiating the Administrator Account and Set Password
     try {
         Enable-LocalUser -Name "Administrator"
-        $Admin_Pass_PA = Read-Host -AsSecureString "Info: Computername - $Computername `nEnter Password for Administrator"
         Set-LocalUser -Name "Administrator" -Password $Admin_Pass_PA -PasswordNeverExpires 1 -AccountNeverExpires
-        Write-Host "Script-INFO: Administrator activated" -ForegroundColor white -BackgroundColor Green
+        Write-Host "Administrator activated" -ForegroundColor white -BackgroundColor Green
         Start-Sleep 3
     }
     catch {
-        Write-Host "Script-INFO: Error activating administrator" -ForegroundColor white -BackgroundColor red
+        Write-Host "Error activating administrator" -ForegroundColor white -BackgroundColor red
+    }
+
+    #Change Hostname
+    try {
+        Rename-Computer -NewName $NewComputerName
+        Write-Host "Computername $NewComputerName has been set. Restart after progress"
+    }
+    catch {
+        Write-Host "Error renaming computer." -ForegroundColor white -BackgroundColor red
+    }
+
+    #Joining Domain
+    try {
+        Add-Computer -DomainName $NewDomainName
+        Write-Host "Domain $NewDomainName has been joined. Restart after progress"
+    }
+    catch {
+        Write-Host "Error joining $NewDomainName" -ForegroundColor white -BackgroundColor red
     }
 
     
-
-    
     Write-Host ...........................................................................................................
-    Write-Host "Script-INFO: This part is finished. `nCleanup-Process will beginn now."
+    Write-Host "This part is finished. `nCleanup-Process will beginn now."
     Write-Host ...........................................................................................................
     Start-Sleep 7
 
     try {
-        Write-Host "Script-INFO: Cleanup is in progress" -ForegroundColor white
+        Write-Host "Cleanup is in progress" -ForegroundColor white
         Start-Sleep 3
         Remove-Item -LiteralPath "$TempDIRFA" -Force -Recurse
-        Write-Host "Script-INFO: Cleanup success" -ForegroundColor white -BackgroundColor Green
+        Write-Host "Cleanup success" -ForegroundColor white -BackgroundColor Green
     }
     catch {
-        Write-Host "Script-INFO: Problem with cleanup" -ForegroundColor white -BackgroundColor Green
+        Write-Host "Problem with cleanup" -ForegroundColor white -BackgroundColor Green
     }
 
     try {
-        Write-Host "Script-INFO: Please login to the Administrator-Profile and proceed with Step 2" -ForegroundColor Red
+        Write-Host "Please login to the Administrator-Profile and proceed with Step 2" -ForegroundColor Red
         $confirmation = Read-Host "Do you want to log off? `nPress 'y' for yes or anything else for no"
         if ($confirmation -eq 'y') {
-            Write-Host "Script-INFO: Logging off..."
+            Write-Host "Logging off..."
             Start-Sleep 10
             quser | Select-Object -Skip 1 | ForEach-Object {
                 $id = ($_ -split ' +')[-5]
@@ -198,41 +200,110 @@ Function First_Step {
 
     }
     catch {
-        Write-Host "Script-INFO: Couldn't log off user" -ForegroundColor white -BackgroundColor Red
+        if ($confirmation -eq 'y') {
+            Write-Host "Logging off..."
+            Start-Sleep 10
+            quser | Select-Object -Skip 1 | ForEach-Object {
+                $id = ($_ -split ' +')[-6]
+                logoff $id 
+            }
+        }
+        else {
+            break
+        }
     }
-
 }
 
 Function Second_Step {
+    Clear-Host
+        Write-Host "-----------------------------------------------------"
+        Write-Host "Creating local temp files and folders" -ForegroundColor White 
+        Write-Host "-----------------------------------------------------"
+        
     if (Test-Path -Path $TempDIR){
-        Write-Host "Script-INFO: C:\Temp exists" -ForegroundColor Green
+        Write-Host 
+        Write-Host "Existing Dir: $TempDIR" -ForegroundColor White 
     }
     else {
         New-Item $TempDIR -ItemType Directory
-        Write-Host "Script-INFO: C:\Temp Directory created" -ForegroundColor Green
+        Write-Host 
+        Write-Host "Created Dir: $TempDIR" -ForegroundColor White 
     }
     if (Test-Path -Path $TempDIRFA){
+        Write-Host "Existing Dir: $TempDIRFA" -ForegroundColor White 
     }
     else {
         New-Item $TempDIRFA -ItemType Directory 
+        Write-Host "Created Dir: $TempDIRFA" -ForegroundColor White
     }
     if (Test-Path -Path $TempDIRFADATA){
+        Write-Host "Existing Dir: $TempDIRFADATA" -ForegroundColor White 
     }
     else {
-        New-Item $TempDIRFADATA -ItemType Directory 
+        New-Item $TempDIRFADATA -ItemType Directory
+        Write-Host "Created Dir: $TempDIRFADATA" -ForegroundColor White
     }
+
     if (Test-Path -Path $TempDIRFAEXE){
+        Write-Host "Existing Dir: $TEMPDIRFAEXE" -ForegroundColor White 
     }
     else {
         New-Item $TempDIRFAEXE -ItemType Directory 
+        Write-Host "Created Dir: $TEMPDIRFAEXE" -ForegroundColor White
     }
 
-    #Installation of HP Support Assistant
-    Write-Host "Script-INFO: HP Notebook gets HP features" -ForegroundColor white -BackgroundColor green
-    Write-Host ..........................................................................
-    Write-Host "Script-INFO: Starting transfering the HP Support Assistant..." -ForegroundColor white
+    Write-Host "-----------------------------------------------------"
+    Write-Host "Finished creating necessery Directorys" -ForegroundColor White 
+    Write-Host "-----------------------------------------------------"
+    
+    #Remove Local Account
+    try {
 
-    Write-Host "Script-INFO: Getting Information about the HP Support Assistant" -ForegroundColor Green
+        Write-Host "Listing Information about local Accounts" -ForegroundColor Green
+        Write-Host "-----------------------------------------------------"
+        $LocalUser = Get-WmiObject -ComputerName $Computername -Class Win32_UserAccount -Filter "LocalAccount=True" | 
+        Select Name, Status, Disabled | Out-Host
+        Write-Host "-----------------------------------------------------"
+        Write-Host "Remove local Accounts if necessary" -ForegroundColor Green
+        Write-Host "-----------------------------------------------------"
+        $RM_Local_User = Read-Host "Enter User to Remove"
+        $Check_Local_User = Get-LocalUser | Where-Object {$_.Name -eq $RM_Local_User}
+        if ( -not $Check_Local_User)
+            {
+            Clear-Host
+            Write-Host "-----------------------------------------------------"
+            Write-Host "Account $RM_Local_User not removed. Not found" -ForegroundColor White -BackGroundColor Red
+            Write-Host "-----------------------------------------------------"
+            break
+            }
+        else {
+            Remove-LocalUser -Name $RM_Local_User 
+        }
+        Start-Sleep 2
+        Clear-Host
+        Write-Host "-----------------------------------------------------"
+        Write-Host "Account $RM_Local_User removed" -ForegroundColor White -BackGroundColor Green
+        Write-Host "-----------------------------------------------------"
+
+    }
+    catch {
+        Clear-Host
+        Write-Host "-----------------------------------------------------"
+        Write-Host "Account $RM_Local_User not removed. Error" -ForegroundColor White -BackGroundColor Red
+        Write-Host "-----------------------------------------------------"
+    }
+    
+
+    #Installation of HP Support Assistant
+    Clear-Host
+    Write-Host "-----------------------------------------------------"
+    Write-Host "HP Notebook gets HP Support Assistant installed" -ForegroundColor Green
+    Write-Host "-----------------------------------------------------"
+    Write-Host "Starting transfering the HP Support Assistant..." -ForegroundColor white
+    Write-Host "Getting Information about the HP Support Assistant" -ForegroundColor Green
+    Start-Sleep 1
+    Write-Host "-----------------------------------------------------"
+    Write-Host "Writing to Files" -ForegroundColor Green
 
     $hp = Invoke-WebRequest "https://hpsa-redirectors.hpcloud.hp.com/common/hpsaredirector.js?_=1663262489609" -UseBasicParsing -OutFile $Tempdw01
     (Get-Content -path $Tempdw01 -Raw) -replace '//return','' | set-content $Tempdw01 #Strings too long, we need to get rid of text
@@ -248,9 +319,9 @@ Function Second_Step {
         Out-File $Tempdw05
     Get-Content $Tempdw05 | foreach {'https://' + $_} | Out-File $Tempdw06
 
-    Write-Host "Script-INFO: Could fetch data from HP Servers" -ForegroundColor Green
+    Write-Host "Could fetch data from HP Servers" -ForegroundColor Green
     Write-Host ..........................................................................
-    Write-Host "Script-INFO: Download is starting soon" -ForegroundColor Green
+    Write-Host "Download is starting soon" -ForegroundColor Green
     start-sleep 3
     try {
         $hp_sa = Get-Content -Path $Tempdw06
@@ -259,18 +330,30 @@ Function Second_Step {
         (gc $Tempdw07) | ? {$_.trim() -ne "" } | set-content $Tempdw07
         Get-Content $Tempdw07 -tail 1 > $Tempdw08
 
-        Write-Host "Script-INFO: Download successfull. Filelocation: C:\Temp" -ForegroundColor Green
+        Write-Host "Download successfull. Filelocation: C:\Temp" -ForegroundColor Green
         Write-Host ..........................................................................
-        Write-Host "Script-INFO: Installation of HP Support Assistant will start now" -ForegroundColor Green
-        Write-Host "Script-INFO: Please have patience" -ForegroundColor Green
+        Write-Host "Installation of HP Support Assistant will start now" -ForegroundColor Green
+        Write-Host "Please have patience" -ForegroundColor Green
         Start-Sleep 3
         $EXE = Get-Content -Path $Tempdw08
         Start-Process -FilePath $EXE -Verb runAs -Wait 
     }
     catch {
-        Write-Host "Script-INFO: Error downloading Information Support Assistant" -ForegroundColor white -BackgroundColor red
-        Write-Host "Script-INFO: Script will proceed without HP Support Assistant" -ForegroundColor white -BackgroundColor Yellow
+        Write-Host "Error downloading Information Support Assistant" -ForegroundColor white -BackgroundColor red
+        Write-Host "Script will proceed without HP Support Assistant" -ForegroundColor white -BackgroundColor Yellow
         Start-Sleep 5
+    }
+
+
+
+    try {
+        Write-Host "Cleanup is in progress" -ForegroundColor white
+        Start-Sleep 3
+        Remove-Item -LiteralPath "$TempDIRFA" -Force -Recurse
+        Write-Host "Cleanup success" -ForegroundColor white -BackgroundColor Green
+    }
+    catch {
+        Write-Host "Problem with cleanup" -ForegroundColor white -BackgroundColor Green
     }
 }
 Function Third_Step {
@@ -282,12 +365,12 @@ do {
     $input = Read-Host "Please make a selection"
     Clear-Host
     switch ($input) {
-        '1' {Add_Hostname;break}
-        '2' {Join_Domain;break}
-        '3' {Join_Workgroup;break}
-        '4' {First_Step;break}
-        '5' {Second_Step;break}
-        '6' {Third_Step;break}
+        '1' {First_Step;break}
+        '2' {Second_Step;break}
+        '3' {Third_Step;break}
+        #'4' {First_Step;break}
+        #'5' {Second_Step;break}
+        #'6' {Third_Step;break}
         'q' {break}
         default{
             Write-Host "You entered '$input'" -ForegroundColor Red
